fa6dc36a0356a242d962a0f4809f60b2047dc86c
Byron <byronblocker@gmail.com>
2017-05-31 10:07:11 -0700

    add second test

--- src/test/java/PrimeSifterTest.java
+++ src/test/java/PrimeSifterTest.java
@@ -13,4 +13,13 @@ public class PrimeSifterTest {
     assertEquals(expectedOutput, testPrimeSifter.runSift(1));
   }
 
+  @Test
+  public void runSift_countTo2_ArrayList(){
+    PrimeSifter testPrimeSifter = new PrimeSifter();
+    List<Object> expectedOutput = new ArrayList<Object>();
+    expectedOutput.add(1);
+    expectedOutput.add(2);
+    assertEquals(expectedOutput, testPrimeSifter.runSift(2));
+  }
+
 }
